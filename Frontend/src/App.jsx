783485eba3f42167a1a32dc1eb839861fd700ace
Parnab Bagchi <pb7439578071@gmail.com>
2025-07-27 23:49:34 +0530

    Testing PR Request Preview 1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Gallery from "./Components/GalleryModule/Gallery";
 import "./App.css";
 import Home from "./Components/HomeModule/Home";
@@ -7,13 +6,11 @@ import Upload from "./Components/UploadModule/Upload";
 import Aboutus from "./Components/AboutModule/Aboutus";
 import Error404 from "./Components/ErrorModule/Error404";
 import Profile from "./Components/ProfileModule/Profile";
-import Footer from "./Components/AboutModule/Footer"; 
-
+// import Footer from "./Components/AboutModule/Footer";
 
 function App() {
     return (
         <Router>
-        
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/gallery/*" element={<Gallery />} />
@@ -22,10 +19,7 @@ function App() {
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/*" element={<Error404 />} />
             </Routes>
-           
-        
-    
-      </Router>
+        </Router>
     );
 }
 
